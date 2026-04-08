cd06dfb7ffc7056dd54e707a6e3ca67a8406b377
Kaushik28525 <kaushikanand2524@gmail.com>
2026-04-08 11:18:38 +0530

    Initial commit - Google Docs Clone

---
+++ server/server.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose")
+const Document = require("./Document")
+
+// ✅ CONNECT TO MONGODB
+mongoose.connect(
+  "mongodb+srv://kaushikanand2524_db_user:kaushik123@cluster0.eiw02io.mongodb.net/google-docs-clone?retryWrites=true&w=majority"
+)
+.then(() => console.log("MongoDB connected ✅"))
+.catch(err => console.error("MongoDB error ❌", err))
+
+// SOCKET SERVER
+const io = require("socket.io")(3001, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+})
+
+const defaultValue = ""
+
+// SOCKET CONNECTION
+io.on("connection", socket => {
+  console.log("Connected to server 🔌")
+
+  socket.on("get-document", async documentId => {
+    console.log("Fetching document:", documentId)
+
+    const document = await findOrCreateDocument(documentId)
+
+    socket.join(documentId)
+    socket.emit("load-document", document.data)
+
+    socket.on("send-changes", delta => {
+      socket.broadcast.to(documentId).emit("receive-changes", delta)
+    })
+
+    socket.on("save-document", async data => {
+      await Document.findByIdAndUpdate(documentId, { data })
+      console.log("Document saved 💾")
+    })
+  })
+})
+
+// CREATE DOCUMENT IF NOT EXISTS
+async function findOrCreateDocument(id) {
+  if (id == null) return
+
+  const document = await Document.findById(id)
+  if (document) return document
+
+  return await Document.create({ _id: id, data: defaultValue })
+}
